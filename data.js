9962ad00512b11dbc0f1abb507d4e870c2e06a22
Eden <71605972+EdensEdge@users.noreply.github.com>
2021-06-25 22:32:54 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,43 @@
+// export // <-- uncomment for ES modules
+// module.exports = // <-- uncomment for CommonJS modules
+function DataClass(name, { props, prototype }) {
+    prototype.__storage__ ??= new Map();
+    let valclass = changename(name, function() {
+        let args, newtarget, myprops;
+        args = [...arguments];
+        newtarget = args[0]; args.shift();
+        if(!newtarget) return new valclass.meta(...args);
+        myprops = [];
+        for (let prop in prototype) {
+            if (props[prop]) {
+                myprops.push(prop);
+                this[prop] = props[prop](args[0]);
+                args.shift();
+            }
+        }
+        let oldmap = null, curmap = prototype.__storage__, tail = myprops.pop();
+        for (let myprop of myprops) {
+            oldmap = curmap;
+            if (curmap.has(this[myprop]))
+                curmap = curmap.get(this[myprop]);
+            else
+                curmap = curmap.set(this[myprop], new Map()).get(this[myprop]);
+        }
+        if (curmap.has(this[tail]))
+            return curmap.get(this[tail]).deref();
+        else
+            curmap.set(this[tail], new WeakRef(this));
+        
+        Object.freeze(this);
+    });
+    valclass.prototype = prototype;
+    valclass.meta = valclass;
+    return valclass;
+}
+
+function changename(name, func) {
+    return (new Function(
+        "f",
+        "return function " + name + "() {return f.call(this, new.target, ...arguments)}"
+    ))(func);
+}
